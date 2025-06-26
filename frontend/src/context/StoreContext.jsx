3340fe5c5a90ed35f3ebe090dc69d4c276b34b9b
Vicky Kumar <105460472+Vickyyyky@users.noreply.github.com>
2025-06-26 17:34:43 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = ({ children }) => {
   const [item_list, setItemList] = useState([]);
   const [token, setToken] = useState("");
 
-  const url = "http://localhost:4000";
+  const url = "https://nexastore1-1.onrender.com";
 
   const addToFav = async (itemId) => {
     setFavItems((prev) => ({
