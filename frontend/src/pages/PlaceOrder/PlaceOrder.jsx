2c997c2fc99d5758482efd846f8ceaf3474ce6fb
Vicky Kumar <105460472+Vickyyyky@users.noreply.github.com>
2025-06-27 02:07:28 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -160,7 +160,7 @@ const PlaceOrder = () => {
 );
     const result = response.data;
 
-    if (result.status === 200) {
+    if (response.status === 200) {
        window.location.href = result.session_url;
       alert('Order placed successfully!');
       
